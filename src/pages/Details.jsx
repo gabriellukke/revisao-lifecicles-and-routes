2ffd06805b14f2433491eec14c86f8129edf42cb
gabriellukke <gabriellukke@outlook.com>
2022-03-04 18:22:53 -0300

    Fix

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,11 +1,57 @@
+import React from 'react';
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
 
-export default class Details extends Component {
+import { fetchByCode } from '../services/getCountryByCode';
+
+class Details extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      country: '',
+    };
+  }
+
+  componentDidMount() {
+    const {
+      match: {
+        params: { code },
+      },
+    } = this.props;
+    fetchByCode(code).then(([country]) => this.setState({ country }));
+  }
 
   render() {
-    return (
-      <div>Details</div>
+    const { country } = this.state;
+
+    const [currency] = Object.keys(country.currencies) || 'Press F to Pay respect';
+
+    return !country ? (
+      'Carregando...'
+    ) : (
+      <>
+        <h1>{country.name.official}</h1>
+        <ul>
+          <li>{`Name: ${country.name.common}`}</li>
+          <li>{`Region: ${country.region}`}</li>
+          <li>{`Capital: ${country.capital[0]}`}</li>
+          <li>{`Population: ${country.population}`}</li>
+          <li>{`Currency: ${
+            country.currencies[currency].name
+          }`}
+          </li>
+        </ul>
+      </>
     );
   }
 }
+
+Details.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      code: PropTypes.string,
+    })
+  }).isRequired,
+};
+
+export default Details;
