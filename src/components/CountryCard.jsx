a77d45ea9219eeb6e9fea5b34ca804154fcfdb53
Arthur Procopio <avprocopio@gmail.com>
2022-03-04 17:02:11 -0300

    Initial config

---
+++ src/components/CountryCard.jsx
@@ -0,0 +1,12 @@
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+
+export default class CountryCard extends Component {
+  static propTypes = {second: third}
+
+  render() {
+    return (
+      <div>CountryCard</div>
+    )
+  }
+}
